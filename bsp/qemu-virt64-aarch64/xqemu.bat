9b234f0f3736296b0cf9aefb5ca923a1d188568b
ccj <ccj@gmail.com>
2026-05-08 11:15:49 +0800

    [linux_boot] linux增加跟文件系统

--- bsp/qemu-virt64-aarch64/xqemu.bat
+++ bsp/qemu-virt64-aarch64/xqemu.bat
@@ -10,6 +10,8 @@ qemu-system-aarch64 ^
 -dtb rtt.dtb ^
 -device loader,file=Image,addr=0x48200000,force-raw=on ^
 -device loader,file=linux.dtb,addr=0x4f000000,force-raw=on ^
+-drive if=none,file=ubuntu-rootfs.img,format=raw,id=vdisk0 ^
+-device virtio-blk-device,drive=vdisk0,bus=virtio-mmio-bus.1 ^
 -nographic ^
 -serial mon:stdio ^
 -chardev socket,host=127.0.0.1,port=4322,server=on,wait=off,telnet=on,id=linux_console ^
